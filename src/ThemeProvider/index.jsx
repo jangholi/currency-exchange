2b6059526d44edbfb783c3a6c807537f46bc7bc8
marjan <marjan.jangholi@yahoo.com>
2021-07-31 16:02:00 +0430

    add new colors and fonts to typography

--- src/ThemeProvider/index.jsx
+++ src/ThemeProvider/index.jsx
@@ -9,6 +9,11 @@ const theme = createTheme({
     secondary: {
       main: '#94c720',
     },
+    text: {
+      primary: '#404040',
+      secondary: '#8D8D8D',
+      hint: '#C70D38',
+    },
   },
 });
 
@@ -17,6 +22,8 @@ const useGlobalStyles = makeStyles({
     body: {
       margin: 0,
       backgroundColor: 'lightgray',
+      fontFamily: 'Roboto',
+      fontSize: '16px',
     },
   },
 });
