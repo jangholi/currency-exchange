84167ab097265bfa5c81507fd365465449113e98
marjan <marjan.jangholi@yahoo.com>
2021-08-01 15:12:25 +0430

    finish conversion history page

--- src/pages/conversion-history/index.jsx
+++ src/pages/conversion-history/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Grid, Box } from '@material-ui/core';
+import ConversionTable from '../../components/tables/conversion-history';
+import PageTitle from '../../components/typography/page-title';
 
 function ConversionHistory() {
   return (
-    <p>ConversionHistory page</p>
+    <Grid item md={12}>
+      <Box mb={2}>
+        <PageTitle>
+          Conversion history
+        </PageTitle>
+      </Box>
+      <Box>
+        <ConversionTable />
+      </Box>
+    </Grid>
   );
 }
 
