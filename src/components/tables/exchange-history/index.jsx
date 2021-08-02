7a6d9646ca108354a751065cc5dbdee460749849
marjan <marjan.jangholi@yahoo.com>
2021-08-02 12:28:03 +0430

    get history as a props from parent in exchange history component

--- src/components/tables/exchange-history/index.jsx
+++ src/components/tables/exchange-history/index.jsx
@@ -2,21 +2,18 @@ import React from 'react';
 import Table from '../history';
 import { formatDate } from '../../../helpers/formatDate';
 
-function ExchangeHistoryTable() {
+function ExchangeHistoryTable({ history = [] }) {
   const [rows, setRows] = React.useState([]);
   const columns = ['Date', 'Exchange Rate'];
 
   React.useEffect(() => {
-    const history = localStorage.getItem('changeHistory');
-    const parseHistory = history ? JSON.parse(history) : [];
-
-    const tableRows = [...rows];
-    parseHistory.forEach((e) => {
+    const tableRows = [];
+    history.forEach((e) => {
       tableRows.push([formatDate(e.date), e.exchangeRate]);
     });
 
     setRows(tableRows);
-  }, []);
+  }, [history]);
 
   return (
     <Table rows={rows} columns={columns} haveAction={false} />
