a08319673579c35b3a4bd201cd610ce72112047a
marjan <marjan.jangholi@yahoo.com>
2021-08-03 00:39:19 +0430

    sort history data by date

--- src/utils/LocalStorageManagement/index.js
+++ src/utils/LocalStorageManagement/index.js
@@ -4,5 +4,7 @@ export const setHistory = (history) => {
 
 export const getHistory = () => {
   const history = localStorage.getItem('history');
-  return history ? JSON.parse(history) : [];
+  const parseHistory = history ? JSON.parse(history) : [];
+  parseHistory.sort((a, b) => a.date - b.date);
+  return parseHistory;
 };
